27dac05b9026233e5689ddb2528d32b8e8ddc0fc
david-morales-33 <davidmorales33.97@gmail.com>
2024-12-27 23:58:05 -0500

    REF: entity <User>

--- src/auth/User/domain/User.ts
+++ src/auth/User/domain/User.ts
@@ -1,32 +1,47 @@
+import { CredentialsPassword } from "../../Credentials/domain/CredentialsPassword";
+import { PasswordService } from "./PasswordService";
 import { UserDTO } from "./UserDTO";
 import { UserEmail } from "./UserEmail";
 import { UserHashedPassword } from "./UserHashedPassword";
 import { UserId } from "./UserId";
 
 export class User {
+    private _hashedPassword?: UserHashedPassword
     constructor(
         public readonly id: UserId,
         public readonly email: UserEmail,
-        public readonly hashedPassword: UserHashedPassword
-    ) { }
+        hashedPassword?: UserHashedPassword
+    ) { this._hashedPassword = hashedPassword; }
+
+    public get hashedPassword(): UserHashedPassword | null {
+        return this._hashedPassword ?? null;
+    }
 
     public static create(
         id: UserId,
         email: UserEmail,
-        hashedPassword: UserHashedPassword
     ): User {
-        return new User(id, email, hashedPassword);
+        return new User(id, email);
     }
 
     public static fromPrimitives(data: UserDTO): User {
         return new User(
             new UserId(data.id),
             new UserEmail(data.email),
-            new UserHashedPassword(data.hashedPassword)
+            data.hashedPassword ? new UserHashedPassword(data.hashedPassword) : undefined
         )
     }
 
+    public async validatePassword(service: PasswordService, password: CredentialsPassword): Promise<boolean> {
+        if (this.hashedPassword === null) throw new Error('Password does not exists')
+        return await service.validate(this.hashedPassword, password);
+    }
+
+    public async encryptPassword(service: PasswordService, password: CredentialsPassword): Promise<void> {
+        this._hashedPassword = await service.encrypt(password);
+    }
+
     toPrimitives(): UserDTO {
-        return new UserDTO(this.id.value, this.email.value, this.hashedPassword.value)
+        return new UserDTO(this.id.value, this.email.value)
     }
 }
--- src/auth/User/domain/UserDTO.ts
+++ src/auth/User/domain/UserDTO.ts
@@ -3,6 +3,6 @@ export class UserDTO {
     constructor(
         public readonly id: string,
         public readonly email: string,
-        public readonly hashedPassword: string
+        public readonly hashedPassword?: string
     ) { }
 }
